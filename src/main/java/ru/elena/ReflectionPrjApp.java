32e34b29cb91ab5e28178221b60a016e96003dac
Elena <lama_elena@mail.ru>
2021-06-09 01:02:59 +0300

    Урок 7. Reflection API и аннотации

--- src/main/java/ru/elena/ReflectionPrjApp.java
+++ src/main/java/ru/elena/ReflectionPrjApp.java
@@ -4,6 +4,12 @@ import ru.elena.animals.Tiger;
 import ru.elena.animals.Walrus;
 
 public class ReflectionPrjApp {
+
+    /*
+    * Для выполнения ДЗ был создан этот проект. Так как сдать на сайте можно только pull-request,
+    * оставляю эту ссылку. Выполненое ДЗ - весь код проекта
+    * */
+
     public static void main(String[] args) {
         System.out.println("******** ТИГР ********");
         Veterinarian.start(Tiger.class);
